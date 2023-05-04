813212382db58a51e0948ad44fd8f47236dc87c3
Alexandr Degtyarev <Sasha20111997@gmail.com>
2023-05-04 16:33:20 +0500

    added task_3 close iScan

--- java_hw/sem_1_java/task_3.java
+++ java_hw/sem_1_java/task_3.java
@@ -9,6 +9,7 @@ public class task_3 {
         Scanner iScanner = new Scanner(System.in, "Cp866"); // (смена кодировки)если после in поставит запятую и в ковычки написать Cp866 кодировщик сможет выводить кириллицу, можно без cp
         System.out.printf("Введите вычеслительную операцию, разделяя символы пробелом: ");
         String oper = iScanner.nextLine(); // принимаем строку, инициализируем переменную oper, для того, чтобы мы могли воспользоваться методом contains и найти нужный нам по условию символ.
+        iScanner.close();
         String[] operArray = oper.split(" "); // создаем стринговый массив operArray для обращения к индексам
             
         if(oper.contains("+") == true){
